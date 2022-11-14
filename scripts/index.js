b901342e2c2936eb7c919405275ec94e65ac0259
Maksim Orzhekhovskiy <smiley.dude@yandex.ru>
2022-11-14 10:10:41 +0500

    hotfix

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,31 @@
 let formElement = document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
 let popupCloseButton = document.querySelector('.popup__close');
 let popupOpenButton = document.querySelector('.profile__edit-button');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let submitForm = document.querySelector('.popup__content');
 
 // POPUP OPEN-CLOSE
 
 popupOpenButton.addEventListener('click', function() {
   formElement.classList.add('popup_opened');
   nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 });
 
-popupCloseButton.addEventListener('click', function() {
-  formElement.classList.remove('popup_opened');
-});
+function closePopup() {
+  formElement.classList.remove('popup_opened')};
+
+popupCloseButton.addEventListener('click', closePopup);
 
 //CHANGE PROFILE INFO
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
+  closePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+submitForm.addEventListener('submit', formSubmitHandler);
